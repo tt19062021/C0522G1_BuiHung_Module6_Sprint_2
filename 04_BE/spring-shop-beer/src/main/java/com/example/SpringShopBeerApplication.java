129d3dce70573b7dab822b42a63d0cabe8de2128
C0522G1-buihung <tt19062021@gmail.com>
2022-12-05 09:05:30 +0700

    HungB

---
+++ 04_BE/spring-shop-beer/src/main/java/com/example/SpringShopBeerApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShopBeerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringShopBeerApplication.class, args);
+    }
+
+}
